3342d307f5a683a511c324047d2b6fa8c11ab251
reut <reutlevy30@gmail.com>
2020-12-25 18:39:54 +0200

    first commit

--- try/server-final.py
+++ try/server-final.py
@@ -23,7 +23,7 @@ def main():
     while True:
 
         for ip in ip_range_list:
-            print(f'sending on {ip}')
+            print(bcolors.OKGREEN+f'sending on {ip}')
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             # sock.bind((ip,0))
