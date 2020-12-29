de31a42127c396f0fe29cb2c42ee0de79f5a4dc5
amit <amitrub@githab.com>
2020-12-29 21:00:44 +0200

    helpppppp

---
+++ helppppppp.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+from readchar import readchar
+
+
+def getchar():
+    print("heloooooo")
+    ch = readchar()
+    if ord(ch) == 3 or 4:
+        return False
+    print("heloooooo1")
+    return ch
+
+ch = getchar()
+print('You pressed', ch)
+
+while not ch is False:
+    ch = getchar()
+    print('You pressed', ch)
